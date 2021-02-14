2de23a0f65a85e532b967ab7c8f78cb752a0a77b
snazau <snazarikov@gmail.com>
2021-02-14 20:24:23 +0200

    Add 3rd step - builtin observer pattern

---
+++ 02_Observer/03_BuiltinObserver/src/com/company/displays/CurrentConditionsDisplay.java
@@ -0,0 +1,35 @@
+package com.company.displays;
+
+import com.company.WeatherData;
+import com.company.interfaces.DisplayElement;
+
+import java.util.Observable;
+import java.util.Observer;
+
+public class CurrentConditionsDisplay implements Observer, DisplayElement {
+    private float temperature = 23;
+    private float humidity = 40;
+    private float pressure = 222;
+    private Observable observable;
+
+    public CurrentConditionsDisplay(Observable observable) {
+        this.observable = observable;
+        this.observable.addObserver(this);
+    }
+
+    @Override
+    public void update(Observable o, Object arg) {
+        if (o instanceof WeatherData) {
+            WeatherData weatherData = (WeatherData) o;
+            temperature = weatherData.getTemperature();
+            humidity = weatherData.getHumidity();
+            pressure = weatherData.getPressure();
+            display();
+        }
+    }
+
+    @Override
+    public void display() {
+        System.out.println("Current conditions: t=" + temperature + " h=" + humidity + " p=" + pressure);
+    }
+}
---
+++ 02_Observer/03_BuiltinObserver/src/com/company/interfaces/DisplayElement.java
@@ -0,0 +1,5 @@
+package com.company.interfaces;
+
+public interface DisplayElement {
+    void display();
+}
