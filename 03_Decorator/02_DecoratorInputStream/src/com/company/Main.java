452f7cc9c60aec050ac15d08e24322313b822158
snazau <30793581+snazau@users.noreply.github.com>
2021-02-27 19:33:51 +0200

    Add 2nd step - file inputs decorator

--- 03_Decorator/02_DecoratorInputStream/src/com/company/Main.java
+++ 03_Decorator/02_DecoratorInputStream/src/com/company/Main.java
@@ -1,8 +1,26 @@
 package com.company;
 
-public class Main {
+import com.company.decorators.LowerCaseInputStream;
 
+import java.io.*;
+
+public class Main {
     public static void main(String[] args) {
-	// write your code here
+        int c;
+        String filepath = "src/com/company/files/test.txt";
+        try {
+            InputStream in = new LowerCaseInputStream(
+                new BufferedInputStream(
+                    new FileInputStream(filepath)
+                )
+            );
+
+            while ((c = in.read()) >= 0) {
+                System.out.print((char) c);
+            }
+        }
+        catch (IOException e) {
+            e.printStackTrace();
+        }
     }
 }
