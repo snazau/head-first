07a5d92795e53ea7ba7a0bd254c02f57482af9fe
snazau <30793581+snazau@users.noreply.github.com>
2021-03-06 20:10:32 +0200

    Add 3rd step - abstract factory

--- 04_Factory/03_AbstractFactory/src/com/company/Main.java
+++ 04_Factory/03_AbstractFactory/src/com/company/Main.java
@@ -1,8 +1,25 @@
 package com.company;
 
-public class Main {
+import com.company.Pizzas.Pizza;
+import com.company.Stores.ChicagoPizzaStore;
+import com.company.Stores.NYPizzaStore;
+import com.company.Stores.PizzaStore;
 
+public class Main {
     public static void main(String[] args) {
-	// write your code here
+        System.out.println("NY Store");
+        PizzaStore nyPizzaStore = new NYPizzaStore();
+        Pizza nyCheesePizza = nyPizzaStore.orderPizza("cheese");
+        System.out.println();
+        Pizza nyClamsPizza = nyPizzaStore.orderPizza("clams");
+
+        System.out.println();
+        System.out.println();
+
+        System.out.println("Chicago Store");
+        PizzaStore chicagoPizzaStore = new ChicagoPizzaStore();
+        Pizza chicagoCheesePizza = chicagoPizzaStore.orderPizza("cheese");
+        System.out.println();
+        Pizza chicagoClamsPizza = chicagoPizzaStore.orderPizza("clams");
     }
 }
