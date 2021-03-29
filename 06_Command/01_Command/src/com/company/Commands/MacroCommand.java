5c85403cf3c8967c182ecd2787831a4084d99cb6
snazau <30793581+snazau@users.noreply.github.com>
2021-03-29 22:55:38 +0200

    Add MacroCommand

---
+++ 06_Command/01_Command/src/com/company/Commands/MacroCommand.java
@@ -0,0 +1,23 @@
+package com.company.Commands;
+
+public class MacroCommand implements Command {
+    public Command[] commands;
+
+    public MacroCommand(Command[] commands) {
+        this.commands = commands;
+    }
+
+    @Override
+    public void execute() {
+        for (Command command : commands) {
+            command.execute();
+        }
+    }
+
+    @Override
+    public void undo() {
+        for (Command command : commands) {
+            command.undo();
+        }
+    }
+}
