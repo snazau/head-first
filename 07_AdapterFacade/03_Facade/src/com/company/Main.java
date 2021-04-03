7c71c849bb7c898dd593f5ed9ba5a78c0e57af87
snazau <30793581+snazau@users.noreply.github.com>
2021-04-03 17:12:57 +0200

    Add 3rd step - Facade

--- 07_AdapterFacade/03_Facade/src/com/company/Main.java
+++ 07_AdapterFacade/03_Facade/src/com/company/Main.java
@@ -1,8 +1,28 @@
 package com.company;
 
-public class Main {
+import com.company.Devices.*;
+import com.company.Facades.HomeTheaterFacade;
 
+public class Main {
     public static void main(String[] args) {
-	// write your code here
+        Amplifier amplifier = new Amplifier();
+        DVDPlayer dvdPlayer = new DVDPlayer();
+        Projector projector = new Projector();
+        TheaterLights lights = new TheaterLights();
+        Screen screen = new Screen();
+        PopcornPopper popper = new PopcornPopper();
+
+        HomeTheaterFacade homeTheater = new HomeTheaterFacade(
+            amplifier,
+            dvdPlayer,
+            projector,
+            lights,
+            screen,
+            popper
+        );
+
+        homeTheater.watchMovie("1 + 1");
+        System.out.println();
+        homeTheater.endMovie();
     }
 }
