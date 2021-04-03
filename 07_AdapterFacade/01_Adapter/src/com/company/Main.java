6851937cc84d0d1d7785e2d0e61e2e5a53f4ca35
snazau <30793581+snazau@users.noreply.github.com>
2021-04-03 16:21:15 +0200

    Add 1st step - adapter pattern

--- 07_AdapterFacade/01_Adapter/src/com/company/Main.java
+++ 07_AdapterFacade/01_Adapter/src/com/company/Main.java
@@ -1,8 +1,46 @@
 package com.company;
 
+import com.company.Adapters.DuckAdapter;
+import com.company.Adapters.TurkeyAdapter;
+import com.company.Birds.SpecificDuck;
+import com.company.Birds.SpecificTurkey;
+import com.company.Interfaces.Duck;
+import com.company.Interfaces.Turkey;
+
 public class Main {
+    static void testDuck(Duck duck) {
+        System.out.println("Testing duck");
+        duck.quack();
+        duck.fly();
+    }
+
+    static void testTurkey(Turkey turkey) {
+        System.out.println("Testing turkey");
+        turkey.gobble();
+        turkey.fly();
+    }
 
     public static void main(String[] args) {
-	// write your code here
+        SpecificDuck duck = new SpecificDuck();
+        SpecificTurkey turkey = new SpecificTurkey();
+
+        Turkey duckAdapter = new DuckAdapter(duck);
+        Duck turkeyAdapter = new TurkeyAdapter(turkey);
+
+        System.out.println("Duck into testDuck");
+        testDuck(duck);
+        System.out.println();
+
+        System.out.println("TurkeyAdapter into testDuck");
+        testDuck(turkeyAdapter);
+        System.out.println();
+
+        System.out.println("Turkey into testTurkey");
+        testTurkey(turkey);
+        System.out.println();
+
+        System.out.println("DuckAdapter into testTurkey");
+        testTurkey(duckAdapter);
+        System.out.println();
     }
 }
