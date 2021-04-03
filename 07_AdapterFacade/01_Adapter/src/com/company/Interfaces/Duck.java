571ada42a08ad1c9ff8356742f9045f1e59fda25
snazau <30793581+snazau@users.noreply.github.com>
2021-04-03 16:20:23 +0200

    Add interfaces

---
+++ 07_AdapterFacade/01_Adapter/src/com/company/Interfaces/Duck.java
@@ -0,0 +1,6 @@
+package com.company.Interfaces;
+
+public interface Duck {
+    void quack();
+    void fly();
+}
---
+++ 07_AdapterFacade/01_Adapter/src/com/company/Interfaces/Turkey.java
@@ -0,0 +1,6 @@
+package com.company.Interfaces;
+
+public interface Turkey {
+    void gobble();
+    void fly();
+}
